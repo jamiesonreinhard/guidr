3255ca1c36f61e5660d2987dbc89251236e844f0
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-03-02 13:24:32 -0700

    npm init in backend folder

--- backend/app.js
+++ backend/app.js
@@ -7,3 +7,39 @@ const cors = require('cors');
 require('dotenv/config');
 const authJwt = require('./helpers/jwt');
 const errorHandler = require('./helpers/error-handler');
+
+app.use(cors());
+app.options('*', cors())
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads');)
+app.use(errorHandler);
+
+//Routes
+const usersRoutes = require('./routes/users');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/users`, usersRoutes);
+
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'eshop-database'
+})
+.then(()=>{
+  console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+  console.log(err);
+})
+
+//Server
+app.listen(3000, ()=>{
+
+  console.log('server is running http://localhost:3000');
+})
